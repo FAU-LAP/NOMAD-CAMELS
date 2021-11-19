aa815f3a52f6d4d5974eeeeb492d71eb5b65161e
Jo-Leh <89379950+Jo-Leh@users.noreply.github.com>
2021-11-19 15:53:48 +0100

    Revert "Updated some part of the MainApp"
    This reverts commit e51d28ab1dc5c2c69608567dae4e81c01e51c58b.

--- MainApp.py
+++ MainApp.py
@@ -4,45 +4,21 @@ from PyQt5.QtCore import QCoreApplication
 from PyQt5.QtWidgets import QApplication, QMainWindow
 from PyQt5.QtGui import QIcon
 
-from utility import exception_hook, load_save_functions
+from utility import exception_hook
 from gui.mainWindow import Ui_MainWindow
 from frontpanels import taskSelector
 
 
 class MainWindow(QMainWindow, Ui_MainWindow):
-    """Main Window for the program. Connects to all the other classes.
-
-    - task: The task to load on initialisation"""
-    def __init__(self, task: str, parent=None):
-        # basic setup
-        self.task = task
+    def __init__(self, task, parent=None):
         super(MainWindow, self).__init__(parent)
         self.setupUi(self)
         self.setWindowTitle('CECS - Configurable Experimental Control System')
         self.setWindowIcon(QIcon('graphics/FAIRmat_S.png'))
+        # taskSel = taskSelector.TaskSelector(self)
+        # taskSel.exec_()
+        # self.task = taskSel.value
 
-        tasklist = load_save_functions.get_task_list()
-        for t in tasklist:
-            self.comboBox_task.addItem(t)
-        self.comboBox_task.setCurrentText(self.task)
-
-        # connecting buttons
-        self.pushButton_save_state.clicked.connect(self.save_state)
-        self.pushButton_load_state.clicked.connect(self.load_state)
-
-        self.load_state(self.task)
-
-    def save_state(self):
-        """Saves the current state in the .task file."""
-
-
-    def load_state(self, task: str):
-        """Loads a specific state of the provided task."""
-
-    def close(self) -> bool:
-        """Calling the save_state method when closing the window."""
-        self.save_state()
-        return super().close()
 
 
 if __name__ == '__main__':
@@ -50,11 +26,12 @@ if __name__ == '__main__':
     app = QCoreApplication.instance()
     if app is None:
         app = QApplication(argv)
-    # First select a task, then start the main window. If no task is selected, abort execution (avoids loading data etc.)
-    task_sel = taskSelector.TaskSelector()
-    task_sel.exec_()
-    task_choice = task_sel.value
+    # ui.showMaximized()
+    # ui.show()
+    taskerSel = taskSelector.TaskSelector()
+    taskerSel.exec_()
+    task_choice = taskerSel.value
     if task_choice is not None:
-        ui = MainWindow(task=task_choice)
+        ui = MainWindow(task_choice)
         ui.show()
         app.exec_()
