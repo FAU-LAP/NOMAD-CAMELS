e342c03880157ff05088b32ac145f643a40e6b8d
Jo-Leh <89379950+Jo-Leh@users.noreply.github.com>
2021-11-19 15:53:51 +0100

    Revert "Starting to set up the basic UI"
    This reverts commit 46ab60e2c66437920c9f6c32de01439d8519b8fd.

--- MainApp.py
+++ MainApp.py
@@ -1,23 +1,22 @@
 from sys import argv, excepthook
 
 from PyQt5.QtCore import QCoreApplication
-from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtWidgets import QApplication
 from PyQt5.QtGui import QIcon
+from PyQt5.uic import loadUiType
 
 from utility import exception_hook
-from gui.mainWindow import Ui_MainWindow
-from frontpanels import taskSelector
+
+QMainWindow, Ui_MainWindow = loadUiType('gui/mainWindow.ui')
 
 
 class MainWindow(QMainWindow, Ui_MainWindow):
-    def __init__(self, task, parent=None):
+    def __init__(self, parent=None):
         super(MainWindow, self).__init__(parent)
         self.setupUi(self)
         self.setWindowTitle('CECS - Configurable Experimental Control System')
         self.setWindowIcon(QIcon('graphics/FAIRmat_S.png'))
-        # taskSel = taskSelector.TaskSelector(self)
-        # taskSel.exec_()
-        # self.task = taskSel.value
+
 
 
 
@@ -26,12 +25,8 @@ if __name__ == '__main__':
     app = QCoreApplication.instance()
     if app is None:
         app = QApplication(argv)
+    ui = MainWindow()
     # ui.showMaximized()
-    # ui.show()
-    taskerSel = taskSelector.TaskSelector()
-    taskerSel.exec_()
-    task_choice = taskerSel.value
-    if task_choice is not None:
-        ui = MainWindow(task_choice)
-        ui.show()
-        app.exec_()
+    ui.show()
+
+    app.exec()
--- gui/ui_compile.bat
+++
@@ -1,2 +0,0 @@
-FOR %%i IN (*.ui) DO echo %%i
-FOR /f "delims=" %%f IN ('dir *.ui /b') DO pyuic5 -o %%~nf.py %%f
