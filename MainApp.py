3761656e8099813a897fda0cee927ab3a57019ae
Jo-Leh <89379950+Jo-Leh@users.noreply.github.com>
2021-11-19 15:53:44 +0100

    Revert ""redoing" the mainWindow"
    This reverts commit 96aa7c081d0ac4996a2c623f9b7b0a2f8f4c0981.

--- MainApp.py
+++ MainApp.py
@@ -6,26 +6,37 @@ from PyQt5.QtGui import QIcon
 
 from utility import exception_hook, load_save_functions
 from gui.mainWindow import Ui_MainWindow
+from frontpanels import taskSelector
 
 
 class MainWindow(QMainWindow, Ui_MainWindow):
     """Main Window for the program. Connects to all the other classes.
 
     - task: The task to load on initialisation"""
-    def __init__(self, parent=None):
+    def __init__(self, task: str, parent=None):
         # basic setup
+        self.task = task
         super(MainWindow, self).__init__(parent)
         self.setupUi(self)
         self.setWindowTitle('CECS - Configurable Experimental Control System')
         self.setWindowIcon(QIcon('graphics/FAIRmat_S.png'))
 
-        self.load_state()
+        tasklist = load_save_functions.get_task_list()
+        for t in tasklist:
+            self.comboBox_task.addItem(t)
+        self.comboBox_task.setCurrentText(self.task)
+
+        # connecting buttons
+        self.pushButton_save_state.clicked.connect(self.save_state)
+        self.pushButton_load_state.clicked.connect(self.load_state)
+
+        self.load_state(self.task)
 
     def save_state(self):
         """Saves the current state in the .task file."""
 
 
-    def load_state(self):
+    def load_state(self, task: str):
         """Loads a specific state of the provided task."""
 
     def close(self) -> bool:
@@ -39,7 +50,11 @@ if __name__ == '__main__':
     app = QCoreApplication.instance()
     if app is None:
         app = QApplication(argv)
-    ui = MainWindow()
-    ui.show()
-    ui.showMaximized()
-    app.exec_()
+    # First select a task, then start the main window. If no task is selected, abort execution (avoids loading data etc.)
+    task_sel = taskSelector.TaskSelector()
+    task_sel.exec_()
+    task_choice = task_sel.value
+    if task_choice is not None:
+        ui = MainWindow(task=task_choice)
+        ui.show()
+        app.exec_()
