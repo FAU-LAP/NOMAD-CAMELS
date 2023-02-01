141a78454d5f9d519c4e526a6cacfea0c22d05ce
A-D-Fuchs <alexander.d.fuchs@fau.de>
2023-02-01 10:23:16 +0100

    Changed settings combo boxes

--- devices/devices_drivers/keithley_237/keithley_237.py
+++ devices/devices_drivers/keithley_237/keithley_237.py
@@ -6,18 +6,18 @@ class subclass(device_class.Device):
     def __init__(self, **kwargs):
         files = []
         req = []
-        super().__init__(name='keithley_237', virtual=False, tags=['DMM', 'voltage', 'current', 'resistance'], directory='keithley_237', ophyd_device=Keithley_237, requirements=req, files=files, ophyd_class_name='Keithley_237', **kwargs)
+        super().__init__(name='keithley_237', virtual=False, tags=['DMM', 'voltage', 'current',], directory='keithley_237', ophyd_device=Keithley_237, requirements=req, files=files, ophyd_class_name='Keithley_237', **kwargs)
         self.config['NPLC'] = 1
-        self.config['optionen'] = "2"
-        self.config['andere'] = "1 nA"
+        self.config['Four_wire'] = False
+        self.config['Averages'] = "1 nA"
 
 
 
 class subclass_config(device_class.Simple_Config):
     def __init__(self, parent=None, data='', settings_dict=None,
                  config_dict=None, ioc_dict=None, additional_info=None):
-        comboBoxes = {"optionen": ["1", "2", "3"],
-                      "andere": ["1 nA", "1 mA"]}
+        comboBoxes = {"Four_wire": [True, False,],
+                      "Averages": ["1 nA", "1 mA"]}
         super().__init__(parent, 'Keithley 237', data, settings_dict,
                          config_dict, ioc_dict, additional_info, comboBoxes=comboBoxes)
         self.comboBox_connection_type.addItem('EPICS: USB-serial')
