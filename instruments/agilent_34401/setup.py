b4b6608bb2085b75cd903f8676bb8b2e5c96c047
Jo-Leh <89379950+Jo-Leh@users.noreply.github.com>
2023-03-13 10:34:52 +0100

    testing setup of visa_support

--- instruments/agilent_34401/setup.py
+++ instruments/agilent_34401/setup.py
@@ -8,5 +8,5 @@ setup(
     author='Johannes Lehmeyer / Alexander Fuchs',
     author_email='johannes.lehmeyer@fau.de',
     packages=find_packages(),
-    install_requires=[]
+    install_requires=['camels_support_visa_signal @ git+https://github.com/FAU-LAP/CAMELS.git@new_device_management#subdirectory=instruments/Support/visa_signal']
 )
