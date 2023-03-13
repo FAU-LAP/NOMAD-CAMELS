9708e5aa6bcd8c4d046884eb7f8d98bf3508f101
Jo-Leh <89379950+Jo-Leh@users.noreply.github.com>
2023-03-13 15:45:10 +0100

    trying to get all drivers on track

--- instruments/keysight_e5270b/setup.py
+++ instruments/keysight_e5270b/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='camels_driver_keysight_e5270',
+    name='camels_driver_keysight_e5270b',
     version='1.0',
     description='This packes provides everything to run the Keysight E5270 series'
                 'source measure unit with CAMELS',
