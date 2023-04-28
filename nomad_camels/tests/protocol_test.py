ea39ee1b941b99d6d32d235092fa6bb894c48844
Jo-Leh <89379950+Jo-Leh@users.noreply.github.com>
2023-04-28 09:01:59 +0200

    Update protocol_test.py

--- nomad_camels/tests/protocol_test.py
+++ nomad_camels/tests/protocol_test.py
@@ -63,13 +63,9 @@ def test_wait(qtbot, tmp_path):
     prot = conf.protocol
     assert 'Wait (Wait)' in prot.loop_step_dict
     assert prot.loop_steps[0].wait_time == '1.0'
-    qtbot.wait_until(lambda x=None: ensure_catalog(tmp_path))
+    make_catalog.make_yml(tmp_path, 'test_catalog')
     run_test_protocol(tmp_path, prot)
 
-
-def ensure_catalog(tmp_path):
-    make_catalog.make_yml(tmp_path, 'test_catalog')
-    assert 'test_catalog' in list(databroker.catalog)
 
 
 def run_test_protocol(tmp_path, protocol):
