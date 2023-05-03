a6c232bb0745fc06a2fe88d0d1ebffb1b75e939a
Jo-Leh <89379950+Jo-Leh@users.noreply.github.com>
2023-05-03 12:23:42 +0200

    added set_variables to if test, now a test for two steps

--- nomad_camels/tests/protocol_test.py
+++ nomad_camels/tests/protocol_test.py
@@ -48,10 +48,7 @@ def test_for_loop(qtbot, tmp_path):
     action.trigger()
     prot = conf.protocol
 
-    model = conf.treeView_protocol_sequence.model()
-    conf.treeView_protocol_sequence.selectionModel().clearSelection()
-    index = getItemIndex(model, 'Wait (Wait)')
-    conf.treeView_protocol_sequence.selectionModel().select(index, QItemSelectionModel.Select)
+    select_step_by_name(conf, 'Wait (Wait)')
 
     def wait_for_move_in():
         qtbot.mouseClick(conf.pushButton_move_step_in, Qt.MouseButton.LeftButton)
@@ -97,71 +94,39 @@ def test_gradient_descent(qtbot, tmp_path):
     catalog_maker(tmp_path)
     run_test_protocol(tmp_path, prot)
 
-def test_if(qtbot, tmp_path):
-    from nomad_camels.loop_steps import if_step
+def test_if_and_set_variables(qtbot, tmp_path):
+    from nomad_camels.loop_steps import set_variables
     conf = protocol_config.Protocol_Config()
+    conf.show()
+    prot = conf.protocol
     qtbot.addWidget(conf)
     qtbot.mouseClick(conf.general_settings.pushButton_add_variable,
                      Qt.MouseButton.LeftButton)
     conf.general_settings.variable_model.item(0, 0).setText('condition')
     conf.general_settings.variable_model.item(0, 1).setText('1')
-    action = get_action_from_name(conf.add_actions, 'If')
-    action.trigger()
-    conf_widge = conf.loop_step_configuration_widget
-    assert isinstance(conf_widge,
-                      if_step.If_Step_Config)
-    conf_widge.sub_widget.lineEdit_condition.setText('condition != 1')
-    conf_widge.sub_widget.elif_table.add('condition == 1')
-    conf_widge.sub_widget.checkBox_use_else.setChecked(True)
-    action = get_action_from_name(conf.add_actions, 'Prompt')
-    action.trigger()
-    prot = conf.protocol
-    model = conf.treeView_protocol_sequence.model()
-    conf.treeView_protocol_sequence.selectionModel().clearSelection()
-    index = getItemIndex(model, 'Prompt (Prompt)')
-    conf.treeView_protocol_sequence.selectionModel().select(index, QItemSelectionModel.Select)
-
-    def wait_for_move():
-        qtbot.mouseClick(conf.pushButton_move_step_in, Qt.MouseButton.LeftButton)
-        qtbot.mouseClick(conf.pushButton_move_step_out, Qt.MouseButton.LeftButton)
-        qtbot.mouseClick(conf.pushButton_move_step_up, Qt.MouseButton.LeftButton)
-        qtbot.mouseClick(conf.pushButton_move_step_up, Qt.MouseButton.LeftButton)
-        qtbot.mouseClick(conf.pushButton_move_step_in, Qt.MouseButton.LeftButton)
-        assert len(prot.loop_steps) == 1
-    qtbot.waitUntil(wait_for_move)
-
-    action = get_action_from_name(conf.add_actions, 'Wait')
-    action.trigger()
-    conf.treeView_protocol_sequence.selectionModel().clearSelection()
-    index = getItemIndex(model, 'Wait (Wait)')
-    conf.treeView_protocol_sequence.selectionModel().select(index, QItemSelectionModel.Select)
-
-    def wait_for_move():
-        qtbot.mouseClick(conf.pushButton_move_step_in, Qt.MouseButton.LeftButton)
-        qtbot.mouseClick(conf.pushButton_move_step_out, Qt.MouseButton.LeftButton)
-        qtbot.mouseClick(conf.pushButton_move_step_up, Qt.MouseButton.LeftButton)
-        qtbot.mouseClick(conf.pushButton_move_step_in, Qt.MouseButton.LeftButton)
-        assert len(prot.loop_steps) == 1
-    qtbot.waitUntil(wait_for_move)
-
-    action = get_action_from_name(conf.add_actions, 'Prompt')
-    action.trigger()
-    conf.treeView_protocol_sequence.selectionModel().clearSelection()
-    index = getItemIndex(model, 'Prompt (Prompt_1)')
-    conf.treeView_protocol_sequence.selectionModel().select(index, QItemSelectionModel.Select)
-
-    def wait_for_move():
-        qtbot.mouseClick(conf.pushButton_move_step_in, Qt.MouseButton.LeftButton)
-        assert len(prot.loop_steps) == 1
-    qtbot.waitUntil(wait_for_move)
-
-
-    with qtbot.waitSignal(conf.accepted) as blocker:
-        conf.accept()
+    single_variable_if(qtbot, conf, 1)
     assert 'If (If)' in prot.loop_step_dict
     assert 'If_Sub (If_condition != 1)' in prot.loop_step_dict
     assert 'Elif_Sub (If_condition == 1)' in prot.loop_step_dict
     assert 'Else_Sub (If_Else)' in prot.loop_step_dict
+
+    action = get_action_from_name(conf.add_actions, 'Set Variables')
+    action.trigger()
+
+    def wait_selection():
+        select_step_by_name(conf, f'Set Variables (Set_Variables)')
+        conf.tree_click_sequence()
+        assert isinstance(conf.loop_step_configuration_widget,
+                          set_variables.Set_Variables_Config)
+    qtbot.waitUntil(wait_selection)
+    conf_widge = conf.loop_step_configuration_widget
+    assert isinstance(conf.loop_step_configuration_widget,
+                      set_variables.Set_Variables_Config)
+    conf_widge.variables_table.add(['condition', '0'])
+
+    single_variable_if(qtbot, conf, 0, 2, 1, 2)
+    with qtbot.waitSignal(conf.accepted) as blocker:
+        conf.accept()
     catalog_maker(tmp_path)
     run_test_protocol(tmp_path, prot)
 
@@ -216,9 +181,6 @@ def test_set_channels(qtbot, tmp_path):
     catalog_maker(tmp_path)
     run_test_protocol(tmp_path, prot)
 
-def test_set_variables():
-    pass
-
 def test_simple_sweep():
     pass
 
@@ -249,6 +211,86 @@ def test_wait(qtbot, tmp_path):
 
 
 
+
+
+
+def single_variable_if(qtbot, conf, wait_in=1, n_prompt=0, n_if=0, len_prot=0):
+    from nomad_camels.loop_steps import if_step
+    action = get_action_from_name(conf.add_actions, 'If')
+    action.trigger()
+
+    def wait_selection():
+        if n_if:
+            select_step_by_name(conf, f'If (If_{n_if})')
+        else:
+            select_step_by_name(conf, f'If (If)')
+        conf.tree_click_sequence()
+        assert isinstance(conf.loop_step_configuration_widget,
+                          if_step.If_Step_Config)
+    qtbot.waitUntil(wait_selection)
+    conf_widge = conf.loop_step_configuration_widget
+    conf_widge.sub_widget.lineEdit_condition.setText('condition != 1')
+    conf_widge.sub_widget.elif_table.add('condition == 1')
+    conf_widge.sub_widget.checkBox_use_else.setChecked(True)
+    nam = 'Prompt' if 0 != wait_in else 'Wait'
+    action = get_action_from_name(conf.add_actions, nam)
+    action.trigger()
+    prot = conf.protocol
+
+    if n_prompt > 0 and nam == 'Prompt':
+        select_step_by_name(conf, f'{nam} ({nam}_{n_prompt})')
+        n_prompt += 1
+    elif nam == 'Wait' and n_if:
+        select_step_by_name(conf, f'{nam} ({nam}_{n_if})')
+    else:
+        select_step_by_name(conf, f'{nam} ({nam})')
+
+    def wait_for_move():
+        qtbot.mouseClick(conf.pushButton_move_step_in, Qt.MouseButton.LeftButton)
+        qtbot.mouseClick(conf.pushButton_move_step_out, Qt.MouseButton.LeftButton)
+        qtbot.mouseClick(conf.pushButton_move_step_up, Qt.MouseButton.LeftButton)
+        qtbot.mouseClick(conf.pushButton_move_step_up, Qt.MouseButton.LeftButton)
+        qtbot.mouseClick(conf.pushButton_move_step_in, Qt.MouseButton.LeftButton)
+        assert len(prot.loop_steps) == 1 + len_prot
+    qtbot.waitUntil(wait_for_move)
+
+
+    nam = 'Prompt' if 1 != wait_in else 'Wait'
+    action = get_action_from_name(conf.add_actions, nam)
+    action.trigger()
+    if n_prompt > 0 and nam == 'Prompt':
+        select_step_by_name(conf, f'{nam} ({nam}_{n_prompt})')
+        n_prompt += 1
+    elif nam == 'Wait' and n_if:
+        select_step_by_name(conf, f'{nam} ({nam}_{n_if})')
+    else:
+        select_step_by_name(conf, f'{nam} ({nam})')
+
+    def wait_for_move():
+        qtbot.mouseClick(conf.pushButton_move_step_in, Qt.MouseButton.LeftButton)
+        qtbot.mouseClick(conf.pushButton_move_step_out, Qt.MouseButton.LeftButton)
+        qtbot.mouseClick(conf.pushButton_move_step_up, Qt.MouseButton.LeftButton)
+        qtbot.mouseClick(conf.pushButton_move_step_in, Qt.MouseButton.LeftButton)
+        assert len(prot.loop_steps) == 1 + len_prot
+    qtbot.waitUntil(wait_for_move)
+
+    action = get_action_from_name(conf.add_actions, 'Prompt')
+    action.trigger()
+    if n_prompt == 0:
+        n_prompt += 1
+    select_step_by_name(conf, f'Prompt (Prompt_{n_prompt})')
+
+    def wait_for_move():
+        qtbot.mouseClick(conf.pushButton_move_step_in, Qt.MouseButton.LeftButton)
+        assert len(prot.loop_steps) == 1 + len_prot
+    qtbot.waitUntil(wait_for_move)
+
+
+def select_step_by_name(conf, name):
+    model = conf.treeView_protocol_sequence.model()
+    conf.treeView_protocol_sequence.selectionModel().clearSelection()
+    index = getItemIndex(model, name)
+    conf.treeView_protocol_sequence.selectionModel().select(index, QItemSelectionModel.Select)
 
 
 def get_row_from_channel_table(name, table):
