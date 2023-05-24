4acc7d0c553774f106ff6f888ceda95e831d09c2
A-D-Fuchs <88835934+A-D-Fuchs@users.noreply.github.com>
2023-05-24 09:41:40 +0200

    Update conf.py
    added copy button to code block

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -18,7 +18,7 @@ author = 'jl'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon', 'myst_parser']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon', 'myst_parser', 'sphinx_copybutton']
 
 templates_path = ['_templates']
 exclude_patterns = []
