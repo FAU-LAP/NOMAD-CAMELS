74716a60ebc7adb857f14b194ee231661317a2e7
Jo-Leh <89379950+Jo-Leh@users.noreply.github.com>
2023-06-27 16:20:26 +0200

    added documentation to evaluation_helper

--- nomad_camels/bluesky_handling/evaluation_helper.py
+++ nomad_camels/bluesky_handling/evaluation_helper.py
@@ -12,8 +12,17 @@ base_namespace.update({'StartTime': starttime,
 
 
 class Evaluator(CallbackBase):
-    """ """
+    """
+    This class can be used to evaluate strings with names known in bluesky at
+    any point, as well inside the run itself.
+    """
     def __init__(self, *args, namespace=None, **kwargs):
+        """
+        Parameters
+        ----------
+        namespace : dict
+            The namespace of the run that is added to the default base_namespace
+        """
         super().__init__(*args, **kwargs)
         self.add_namespace = {}
         if namespace is not None:
@@ -24,38 +33,56 @@ class Evaluator(CallbackBase):
 
     def eval_string(self, eval_str:str):
         """
+        Evaluates the given string through the provided namespace to another
+        string. The string is split at quotation marks and the parts of the
+        string are tried to be evaluated. If that is not possible, they are just
+        added back to the string as is.
+        Checks if the namespace is up to date before evaluating.
 
         Parameters
         ----------
-        eval_str:str :
+        eval_str : str
+            The string to be evaluated.
             
 
         Returns
         -------
+        return_str
+            The evaluated string.
+
+        Raises
+        ------
+        ValueError
+            If there is a bigger problem.
 
         """
         self.update_namespace()
         try:
-            reter = ''
+            return_str = ''
             for part in eval_str.split('"' if '"' in eval_str else "'"):
                 try:
-                    reter += str(eval(part, self.namespace))
+                    return_str += str(eval(part, self.namespace))
                 except:
-                    reter += part
-            return reter
+                    return_str += part
+            return return_str
         except Exception as err:
             raise ValueError(f"Could not evaluate {eval_str!r}") from err
 
     def eval(self, eval_str:str):
         """
+        Evaluates the string within the given namespace.
+        Checks if the namespace is up to date before evaluating.
 
         Parameters
         ----------
-        eval_str:str :
+        eval_str : str
+            The string to be evaluated.
             
 
         Returns
         -------
+        Value
+            The evaluated value.
 
         """
         self.update_namespace()
@@ -63,30 +90,19 @@ class Evaluator(CallbackBase):
 
     def start(self, doc):
         """
-
-        Parameters
-        ----------
-        doc :
-
-
-        Returns
-        -------
-
+        Overwrites the `start` method to add the variable 'StartTime' to the
+        namespace.
+        See bluesky's documentation for more information.
         """
         self.namespace['StartTime'] = doc['time']
         super().start(doc)
 
     def event(self, doc):
         """
-
-        Parameters
-        ----------
-        doc :
-            
-
-        Returns
-        -------
-
+        Overwrites the `event` method to add the variable 'time' to the
+        namespace and update the `last_update` attribute for checking before
+        evaluation.
+        See bluesky's documentation for more information.
         """
         self.namespace['time'] = doc['time']
         self.namespace.update(doc['data'])
@@ -94,26 +110,26 @@ class Evaluator(CallbackBase):
         self.last_update = doc['time']
 
     def update_namespace(self):
-        """ """
+        """
+        Updates the namespace with the `add_namespace` dictionary. Since this is
+        exactly the given namespace from __init__, it can be easily updated from
+        outside the class.
+        Also the variable 'ElapsedTime' is added to the namespace.
+        """
         self.namespace.update(self.add_namespace)
         self.namespace['ElapsedTime'] = time.time() - self.namespace['StartTime']
 
     def is_to_date(self, t):
         """
-
-        Parameters
-        ----------
-        t :
-            
-
-        Returns
-        -------
-
+        Compares whether the given `t` is the same as the Evaluator's
+        last_update.
         """
         return self.last_update == t
 
 
 def get_eval(eval_str, namespace):
+    """This evaluates the given string within the given namespace.
+    Most functionality is taken from bluesky.utils.call_or_eval_one."""
     # If it is a key in our namespace, look it up.
     try:
         # This handles field or stream names that are not valid
