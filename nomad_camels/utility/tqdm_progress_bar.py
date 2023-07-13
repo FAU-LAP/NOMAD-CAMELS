1a4cffa608aa4b316e0374487468c3ea555fb84e
Jo-Leh <89379950+Jo-Leh@users.noreply.github.com>
2023-07-13 09:48:28 +0200

    documentation for tqdm_progress_bar and update_camels

--- nomad_camels/utility/tqdm_progress_bar.py
+++ nomad_camels/utility/tqdm_progress_bar.py
@@ -1,22 +1,31 @@
 from tqdm import tqdm
 
 class ProgressBar:
-    """ """
+    """Provides a progress bar for protocols run directly from the script.
+    With the `emit` method, the bar can be updated the same way signals are
+    used when running the protocol from the UI.
+
+    Attributes
+    ----------
+    n_steps : int
+        the number of total (expected) steps of the protocol
+    bar : tqdm
+        the actual progress bar
+    """
     def __init__(self, n_steps):
         self.n_steps = n_steps
         self.bar = tqdm(total=n_steps)
 
     def emit(self, n):
         """
+        Updates the progress bar to the next step. The parameter `n` is only so
+        the function has the same shape as the `emit` function for the signal
+        used with the main UI.
 
         Parameters
         ----------
-        n :
-            
-
-        Returns
-        -------
-
+        n : int
+            has no function, only there to be able to give this method a value
         """
         self.bar.update()
 
--- nomad_camels/utility/update_camels.py
+++ nomad_camels/utility/update_camels.py
@@ -1,8 +1,12 @@
+"""A module used to update CAMELS. The import of nomad_camels is necessary,
+since otherwise it would not show in the imported distributions for reading its
+current version."""
+
 import os
 import sys
 import subprocess
 from importlib.metadata import distributions
-import nomad_camels
+import nomad_camels  # has to be imported for the distribution version number!
 import requests
 from nomad_camels.ui_widgets import warn_popup
 
@@ -10,28 +14,31 @@ from PySide6.QtWidgets import QMessageBox
 
 
 def get_version():
-    """ """
+    """Goes through all imported distributions and returns the version of
+    nomad-camels"""
     for d in distributions():
         if d.metadata['Name'] == 'nomad-camels':
             return d.version
     return None
 
 def update_camels():
-    """ """
+    """Calls a subprocess that updates CAMELS via pip."""
     subprocess.check_call([sys.executable, '-m', 'pip', 'install',
                            '--no-cache-dir', 'nomad-camels', '--upgrade',])
 
 def question_message_box(parent=None):
     """
+    Retrieves the currently installed version and the available version of
+    nomad-camels. If the are the same, a message is displayed, and function
+    exits. Otherwise, a question-box appears, whether the user wants to install
+    the newer version of CAMELS. If yes, first `update_camels` and then
+    `restart_camels` are called.
 
     Parameters
     ----------
-    parent :
-         (Default value = None)
-
-    Returns
-    -------
-
+    parent : QWidget
+        (Default value = None)
+        The parent widget to be used for the message boxes.
     """
     installed_version = get_version()
     url = f'https://raw.githubusercontent.com/FAU-LAP/NOMAD-CAMELS/development/nomad_camels_version.txt'
@@ -52,7 +59,18 @@ def question_message_box(parent=None):
     restart_camels(parent)
 
 def restart_camels(parent=None, ask_restart=True):
-    """ """
+    """Restarts CAMELS. If `ask_restart`, a question-messagebox appears first,
+    to make sure the user actually wants to restart.
+
+    Parameters
+    ----------
+    parent : QWidget
+        (Default value = None)
+        The parent widget to be used for the message boxes.
+    ask_restart : bool
+        (Default value = True)
+        If True, the user is asked whether to restart CAMELS.
+    """
     if ask_restart:
         restart_dialog = QMessageBox.question(parent, 'Restart NOMAD-CAMELS now?',
                                               'Do you want to restart NOMAD-CAMELS now?',
@@ -62,7 +80,8 @@ def restart_camels(parent=None, ask_restart=True):
     os.execl(sys.executable, sys.executable, *sys.argv)
 
 def check_up_to_date():
-    """ """
+    """Gets the installed and available version of CAMELS and checks whether
+    they are the same. Returns the outcome as a bool."""
     installed_version = get_version()
     url = f'https://raw.githubusercontent.com/FAU-LAP/NOMAD-CAMELS/development/nomad_camels_version.txt'
     available_version = requests.get(url).text
@@ -70,18 +89,18 @@ def check_up_to_date():
 
 def auto_update(parent):
     """
+    Called if auto-update is set in the preferences. If the currently installed
+    version is not up to date, `question_message_box` is called.
 
     Parameters
     ----------
-    parent :
-        
-
-    Returns
-    -------
-
+    parent : QWidget
+        (Default value = None)
+        The parent widget to be used for the message boxes.
     """
     if not check_up_to_date():
         question_message_box(parent)
+
 
 if __name__ == '__main__':
     update_camels()
