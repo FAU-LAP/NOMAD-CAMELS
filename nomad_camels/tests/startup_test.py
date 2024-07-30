d976c4f24c88a6c87d79f0d743a8f13b7ac83b84
Jo-Leh <89379950+Jo-Leh@users.noreply.github.com>
2024-07-30 14:28:16 +0200

    a little more tests

--- nomad_camels/tests/startup_test.py
+++ nomad_camels/tests/startup_test.py
@@ -1,9 +1,19 @@
+import pytest
 from unittest.mock import patch
 import tempfile
 from PySide6.QtWidgets import QApplication
 import sys
 from nomad_camels.MainApp_v2 import MainWindow
-from nomad_camels.CAMELS_start import start_camels
+from nomad_camels.CAMELS_start import start_camels, LoadingScreen
+
+
+class MockDialog:
+    def __init__(self, *args, **kwargs):
+        # Create a temporary directory for the test
+        self.path = tempfile.mkdtemp()
+
+    def exec(self):
+        return True
 
 
 def test_startup_of_mainapp(qtbot, capfd):
@@ -12,15 +22,6 @@ def test_startup_of_mainapp(qtbot, capfd):
     the statement "current state saved!" should be printed"""
     # import sys
     from nomad_camels.utility import exception_hook
-
-    class MockDialog:
-        def __init__(self, *args, **kwargs):
-            # Create a temporary directory for the test
-            with tempfile.TemporaryDirectory() as temp_dir:
-                self.path = temp_dir
-
-        def exec(self):
-            return True
 
     def close_save_message():
         """ """
@@ -40,14 +41,6 @@ def test_startup_of_mainapp(qtbot, capfd):
 def test_start_camels(qtbot, capfd):
     """Test the startup of the CAMELS application."""
 
-    class MockDialog:
-        def __init__(self, *args, **kwargs):
-            # Create a temporary directory for the test
-            self.path = tempfile.mkdtemp()
-
-        def exec(self):
-            return True
-
     def close_save_message():
         """ """
         main_window.change_catalog_name()
@@ -60,12 +53,10 @@ def test_start_camels(qtbot, capfd):
         "nomad_camels.ui_widgets.path_button_edit.Path_Button_Dialog", new=MockDialog
     ):
         with patch("nomad_camels.CAMELS_start.QApplication.exec", return_value=None):
-            app = QApplication.instance()
-            if app is None:
-                app = QApplication(sys.argv)
 
             # Start the CAMELS application
             start_camels()
+            app = QApplication.instance()
 
             # Check if the main window is displayed
             app.processEvents()
@@ -85,3 +76,69 @@ def test_start_camels(qtbot, capfd):
             # Ensure the main window is closed properly
             main_window.close()
             qtbot.waitUntil(lambda: not main_window.isVisible())
+
+
+def test_start_camels_again(qtbot, capfd):
+    """Test the startup of the CAMELS application."""
+
+    def close_save_message():
+        """ """
+        main_window.change_catalog_name()
+        main_window.close()
+        out, err = capfd.readouterr()
+        print(out)
+        assert "current state saved!" in out
+
+    with patch(
+        "nomad_camels.ui_widgets.path_button_edit.Path_Button_Dialog", new=MockDialog
+    ):
+        with patch("nomad_camels.CAMELS_start.QApplication.exec", return_value=None):
+
+            # Start the CAMELS application
+            start_camels()
+            app = QApplication.instance()
+
+            # Check if the main window is displayed
+            app.processEvents()
+            main_window = None
+            widgets = app.topLevelWidgets()
+            for widget in widgets:
+                if isinstance(widget, MainWindow):
+                    main_window = widget
+                    break
+            assert main_window is not None
+            assert (
+                main_window.windowTitle()
+                == "NOMAD CAMELS - Configurable Application for Measurements, Experiments and Laboratory-Systems"
+            )
+            qtbot.waitUntil(close_save_message)
+
+            # Ensure the main window is closed properly
+            main_window.close()
+            qtbot.waitUntil(lambda: not main_window.isVisible())
+
+
+@pytest.fixture(scope="module")
+def app():
+    app = QApplication.instance()
+    if app is None:
+        app = QApplication([])
+    yield app
+    if app is not None:
+        app.quit()
+
+
+@pytest.fixture
+def instance(app):
+    return LoadingScreen()  # Replace with the actual class name
+
+
+def test_set_progress(instance):
+    instance.set_progress(50)
+    assert instance.progress_bar.value() == 50
+
+
+def test_set_text(instance):
+    test_text = "Test text"
+    instance.set_text(test_text)
+    assert instance.label.text() == test_text
