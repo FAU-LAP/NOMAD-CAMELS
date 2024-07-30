0798718e09bf4dc4f04e55dd2dd0665514910a73
Jo-Leh <89379950+Jo-Leh@users.noreply.github.com>
2024-07-30 12:34:22 +0200

    new try for test fix

--- nomad_camels/tests/api_test.py
+++ nomad_camels/tests/api_test.py
@@ -44,6 +44,7 @@ def client_and_thread():
 
     # Stop the server after tests are done
     thread.stop_server()
+    thread.join()
 
 
 # Test to check if the root URL redirects to the documentation page
@@ -70,7 +71,8 @@ def test_run_protocol(client_and_thread):
     # Patch the emit method of the start_protocol signal
     with patch.object(thread.start_protocol_signal, "emit") as mock_emit:
         response = client.get(
-            f"/api/v1/actions/run/protocols/{protocol_name}", auth=("user", "valid_api_key")
+            f"/api/v1/actions/run/protocols/{protocol_name}",
+            auth=("user", "valid_api_key"),
         )
         assert response.status_code == status.HTTP_200_OK
         assert response.json() == {"status": "success"}
@@ -90,4 +92,7 @@ def test_favicon(client_and_thread):
     client, thread = client_and_thread
     response = client.get("/favicon.ico")
     assert response.status_code == status.HTTP_200_OK
-    assert response.headers["content-type"] in ["image/x-icon", "image/vnd.microsoft.icon"]
+    assert response.headers["content-type"] in [
+        "image/x-icon",
+        "image/vnd.microsoft.icon",
+    ]
