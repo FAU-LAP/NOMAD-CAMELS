b7970ffbb2b29d750a46f395fb9f20a96e42dde9
A-D-Fuchs <alexander.d.fuchs@fau.de>
2024-09-04 09:11:58 +0200

    added functions for the API Call loop step

--- nomad_camels/bluesky_handling/helper_functions.py
+++ nomad_camels/bluesky_handling/helper_functions.py
@@ -34,6 +34,8 @@ import os
 import sys
 import glob
 
+from nomad_camels.bluesky_handling.loop_step_functions_api_call import execute_camels_api_call, execute_generic_api_call
+
 
 def get_newest_file(directory):
     """
