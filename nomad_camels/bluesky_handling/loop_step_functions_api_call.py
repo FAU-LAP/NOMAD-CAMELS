e42f1d52a96ea3071e962b7042e16d948b3f8719
A-D-Fuchs <alexander.d.fuchs@fau.de>
2024-09-04 09:12:26 +0200

    Created a new file that contains the functions used b the Call API loop step

---
+++ nomad_camels/bluesky_handling/loop_step_functions_api_call.py
@@ -0,0 +1,13 @@
+def execute_camels_api_call(host, port, api_type, post_body, authentication_type, authentication_string):
+    """
+    This function executes the CAMELS API call and returns the results.
+    """
+    # This function is not implemented yet.
+    return None
+
+def execute_generic_api_call(host, port, api_type, post_body, authentication_type, authentication_string):
+    """
+    This function executes the generic API call and returns the results.
+    """
+    # This function is not implemented yet.
+    return None
