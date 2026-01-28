ee38e8aa67c7ad88ffb2f0a9a1789770173ae56f
Tanmay2028 <tanmay.kulkarni@fau.de>
2026-01-28 19:29:39 +0100

    Move 'write hdf5 during run' to Advanced tab

--- nomad_camels/gui/general_protocol_settings.py
+++ nomad_camels/gui/general_protocol_settings.py
@@ -18,7 +18,7 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
 from PySide6.QtWidgets import (QApplication, QCheckBox, QComboBox, QFrame,
     QGridLayout, QHBoxLayout, QHeaderView, QLabel,
     QLineEdit, QPushButton, QSizePolicy, QSpacerItem,
-    QTabWidget, QTextEdit, QWidget)
+    QTabWidget, QTextEdit, QVBoxLayout, QWidget)
 
 from nomad_camels.frontpanels.flyer_window import FlyerButton
 from nomad_camels.ui_widgets.path_button_edit import Path_Button_Edit
@@ -29,7 +29,7 @@ class Ui_Protocol_Settings(object):
     def setupUi(self, Protocol_Settings):
         if not Protocol_Settings.objectName():
             Protocol_Settings.setObjectName(u"Protocol_Settings")
-        Protocol_Settings.resize(446, 550)
+        Protocol_Settings.resize(445, 550)
         self.gridLayout = QGridLayout(Protocol_Settings)
         self.gridLayout.setObjectName(u"gridLayout")
         self.gridLayout.setContentsMargins(0, 0, 9, 0)
@@ -39,6 +39,11 @@ class Ui_Protocol_Settings(object):
         self.general.setObjectName(u"general")
         self.gridLayout_3 = QGridLayout(self.general)
         self.gridLayout_3.setObjectName(u"gridLayout_3")
+        self.checkBox_csv_exp = QCheckBox(self.general)
+        self.checkBox_csv_exp.setObjectName(u"checkBox_csv_exp")
+
+        self.gridLayout_3.addWidget(self.checkBox_csv_exp, 4, 0, 1, 1)
+
         self.label_3 = QLabel(self.general)
         self.label_3.setObjectName(u"label_3")
         font = QFont()
@@ -47,51 +52,33 @@ class Ui_Protocol_Settings(object):
 
         self.gridLayout_3.addWidget(self.label_3, 1, 0, 1, 2)
 
-        self.textEdit_desc_protocol = QTextEdit(self.general)
-        self.textEdit_desc_protocol.setObjectName(u"textEdit_desc_protocol")
+        self.checkBox_live_comments = QCheckBox(self.general)
+        self.checkBox_live_comments.setObjectName(u"checkBox_live_comments")
 
-        self.gridLayout_3.addWidget(self.textEdit_desc_protocol, 10, 0, 1, 2)
+        self.gridLayout_3.addWidget(self.checkBox_live_comments, 8, 0, 1, 2)
 
-        self.checkBox_csv_exp = QCheckBox(self.general)
-        self.checkBox_csv_exp.setObjectName(u"checkBox_csv_exp")
+        self.checkBox_NeXus = QCheckBox(self.general)
+        self.checkBox_NeXus.setObjectName(u"checkBox_NeXus")
+        self.checkBox_NeXus.setChecked(False)
 
-        self.gridLayout_3.addWidget(self.checkBox_csv_exp, 5, 0, 1, 1)
+        self.gridLayout_3.addWidget(self.checkBox_NeXus, 5, 0, 1, 2)
 
         self.checkBox_json_exp = QCheckBox(self.general)
         self.checkBox_json_exp.setObjectName(u"checkBox_json_exp")
 
-        self.gridLayout_3.addWidget(self.checkBox_json_exp, 5, 1, 1, 1)
+        self.gridLayout_3.addWidget(self.checkBox_json_exp, 4, 1, 1, 1)
 
-        self.label_5 = QLabel(self.general)
-        self.label_5.setObjectName(u"label_5")
-        self.label_5.setFont(font)
+        self.textEdit_desc_protocol = QTextEdit(self.general)
+        self.textEdit_desc_protocol.setObjectName(u"textEdit_desc_protocol")
 
-        self.gridLayout_3.addWidget(self.label_5, 8, 0, 1, 2)
+        self.gridLayout_3.addWidget(self.textEdit_desc_protocol, 9, 0, 1, 2)
 
         self.line_2 = QFrame(self.general)
         self.line_2.setObjectName(u"line_2")
         self.line_2.setFrameShape(QFrame.Shape.HLine)
         self.line_2.setFrameShadow(QFrame.Shadow.Sunken)
 
-        self.gridLayout_3.addWidget(self.line_2, 7, 0, 1, 2)
-
-        self.checkBox_NeXus = QCheckBox(self.general)
-        self.checkBox_NeXus.setObjectName(u"checkBox_NeXus")
-        self.checkBox_NeXus.setChecked(False)
-
-        self.gridLayout_3.addWidget(self.checkBox_NeXus, 6, 0, 1, 2)
-
-        self.comboBox_h5 = QComboBox(self.general)
-        self.comboBox_h5.addItem("")
-        self.comboBox_h5.addItem("")
-        self.comboBox_h5.setObjectName(u"comboBox_h5")
-
-        self.gridLayout_3.addWidget(self.comboBox_h5, 3, 0, 1, 2)
-
-        self.checkBox_live_comments = QCheckBox(self.general)
-        self.checkBox_live_comments.setObjectName(u"checkBox_live_comments")
-
-        self.gridLayout_3.addWidget(self.checkBox_live_comments, 9, 0, 1, 2)
+        self.gridLayout_3.addWidget(self.line_2, 6, 0, 1, 2)
 
         self.horizontalLayout_3 = QHBoxLayout()
         self.horizontalLayout_3.setObjectName(u"horizontalLayout_3")
@@ -111,6 +98,12 @@ class Ui_Protocol_Settings(object):
 
 
         self.gridLayout_3.addLayout(self.horizontalLayout_3, 2, 0, 1, 2)
+
+        self.label_5 = QLabel(self.general)
+        self.label_5.setObjectName(u"label_5")
+        self.label_5.setFont(font)
+
+        self.gridLayout_3.addWidget(self.label_5, 7, 0, 1, 2)
 
         self.tabWidget.addTab(self.general, "")
         self.plot_widge = QWidget()
@@ -192,81 +185,88 @@ class Ui_Protocol_Settings(object):
         self.tabWidget.addTab(self.variables, "")
         self.advanced = QWidget()
         self.advanced.setObjectName(u"advanced")
-        self.gridLayout_2 = QGridLayout(self.advanced)
-        self.gridLayout_2.setObjectName(u"gridLayout_2")
-        self.label_7 = QLabel(self.advanced)
-        self.label_7.setObjectName(u"label_7")
-        self.label_7.setFont(font)
+        self.verticalLayout = QVBoxLayout(self.advanced)
+        self.verticalLayout.setObjectName(u"verticalLayout")
+        self.comboBox_h5 = QComboBox(self.advanced)
+        self.comboBox_h5.addItem("")
+        self.comboBox_h5.addItem("")
+        self.comboBox_h5.setObjectName(u"comboBox_h5")
 
-        self.gridLayout_2.addWidget(self.label_7, 4, 0, 1, 1)
+        self.verticalLayout.addWidget(self.comboBox_h5)
+
+        self.label_6 = QLabel(self.advanced)
+        self.label_6.setObjectName(u"label_6")
+        self.label_6.setFont(font)
+
+        self.verticalLayout.addWidget(self.label_6)
+
+        self.pushButton_instrument_aliases = QPushButton(self.advanced)
+        self.pushButton_instrument_aliases.setObjectName(u"pushButton_instrument_aliases")
+
+        self.verticalLayout.addWidget(self.pushButton_instrument_aliases)
 
         self.line = QFrame(self.advanced)
         self.line.setObjectName(u"line")
         self.line.setFrameShape(QFrame.Shape.HLine)
         self.line.setFrameShadow(QFrame.Shadow.Sunken)
 
-        self.gridLayout_2.addWidget(self.line, 3, 0, 1, 1)
+        self.verticalLayout.addWidget(self.line)
+
+        self.label_7 = QLabel(self.advanced)
+        self.label_7.setObjectName(u"label_7")
+        self.label_7.setFont(font)
+
+        self.verticalLayout.addWidget(self.label_7)
+
+        self.checkBox_no_config = QCheckBox(self.advanced)
+        self.checkBox_no_config.setObjectName(u"checkBox_no_config")
+
+        self.verticalLayout.addWidget(self.checkBox_no_config)
 
         self.line_3 = QFrame(self.advanced)
         self.line_3.setObjectName(u"line_3")
         self.line_3.setFrameShape(QFrame.Shape.HLine)
         self.line_3.setFrameShadow(QFrame.Shadow.Sunken)
 
-        self.gridLayout_2.addWidget(self.line_3, 6, 0, 1, 1)
+        self.verticalLayout.addWidget(self.line_3)
 
-        self.checkBox_no_config = QCheckBox(self.advanced)
-        self.checkBox_no_config.setObjectName(u"checkBox_no_config")
+        self.label_9 = QLabel(self.advanced)
+        self.label_9.setObjectName(u"label_9")
+        self.label_9.setFont(font)
 
-        self.gridLayout_2.addWidget(self.checkBox_no_config, 5, 0, 1, 1)
+        self.verticalLayout.addWidget(self.label_9)
 
-        self.label_6 = QLabel(self.advanced)
-        self.label_6.setObjectName(u"label_6")
-        self.label_6.setFont(font)
+        self.flyer_button = FlyerButton(self.advanced)
+        self.flyer_button.setObjectName(u"flyer_button")
 
-        self.gridLayout_2.addWidget(self.label_6, 0, 0, 1, 1)
-
-        self.pushButton_instrument_aliases = QPushButton(self.advanced)
-        self.pushButton_instrument_aliases.setObjectName(u"pushButton_instrument_aliases")
-
-        self.gridLayout_2.addWidget(self.pushButton_instrument_aliases, 1, 0, 1, 1)
+        self.verticalLayout.addWidget(self.flyer_button)
 
         self.line_4 = QFrame(self.advanced)
         self.line_4.setObjectName(u"line_4")
         self.line_4.setFrameShape(QFrame.Shape.HLine)
         self.line_4.setFrameShadow(QFrame.Shadow.Sunken)
 
-        self.gridLayout_2.addWidget(self.line_4, 9, 0, 1, 1)
-
-        self.checkBox_perform_at_end = QCheckBox(self.advanced)
-        self.checkBox_perform_at_end.setObjectName(u"checkBox_perform_at_end")
-
-        self.gridLayout_2.addWidget(self.checkBox_perform_at_end, 12, 0, 1, 1)
-
-        self.label_9 = QLabel(self.advanced)
-        self.label_9.setObjectName(u"label_9")
-        self.label_9.setFont(font)
-
-        self.gridLayout_2.addWidget(self.label_9, 7, 0, 1, 1)
-
-        self.verticalSpacer_3 = QSpacerItem(20, 40, QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Expanding)
-
-        self.gridLayout_2.addItem(self.verticalSpacer_3, 15, 0, 1, 1)
+        self.verticalLayout.addWidget(self.line_4)
 
         self.label_8 = QLabel(self.advanced)
         self.label_8.setObjectName(u"label_8")
         self.label_8.setFont(font)
 
-        self.gridLayout_2.addWidget(self.label_8, 11, 0, 1, 1)
+        self.verticalLayout.addWidget(self.label_8)
+
+        self.checkBox_perform_at_end = QCheckBox(self.advanced)
+        self.checkBox_perform_at_end.setObjectName(u"checkBox_perform_at_end")
+
+        self.verticalLayout.addWidget(self.checkBox_perform_at_end)
 
         self.ending_protocol_selection = Path_Button_Edit(self.advanced)
         self.ending_protocol_selection.setObjectName(u"ending_protocol_selection")
 
-        self.gridLayout_2.addWidget(self.ending_protocol_selection, 14, 0, 1, 1)
+        self.verticalLayout.addWidget(self.ending_protocol_selection)
 
-        self.flyer_button = FlyerButton(self.advanced)
-        self.flyer_button.setObjectName(u"flyer_button")
+        self.verticalSpacer_3 = QSpacerItem(20, 40, QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Expanding)
 
-        self.gridLayout_2.addWidget(self.flyer_button, 8, 0, 1, 1)
+        self.verticalLayout.addItem(self.verticalSpacer_3)
 
         self.tabWidget.addTab(self.advanced, "")
 
@@ -309,15 +309,14 @@ class Ui_Protocol_Settings(object):
 
     def retranslateUi(self, Protocol_Settings):
         Protocol_Settings.setWindowTitle(QCoreApplication.translate("Protocol_Settings", u"Form", None))
-        self.label_3.setText(QCoreApplication.translate("Protocol_Settings", u"Saving:", None))
         self.checkBox_csv_exp.setText(QCoreApplication.translate("Protocol_Settings", u"Export data to csv", None))
-        self.checkBox_json_exp.setText(QCoreApplication.translate("Protocol_Settings", u"Export metadata to json", None))
-        self.label_5.setText(QCoreApplication.translate("Protocol_Settings", u"Descriptions:", None))
-        self.checkBox_NeXus.setText(QCoreApplication.translate("Protocol_Settings", u"also write NeXus entry to data", None))
-        self.comboBox_h5.setItemText(0, QCoreApplication.translate("Protocol_Settings", u"write hdf5 during run", None))
-        self.comboBox_h5.setItemText(1, QCoreApplication.translate("Protocol_Settings", u"write hdf5 after run", None))
-
+        self.label_3.setText(QCoreApplication.translate("Protocol_Settings", u"Saving:", None))
         self.checkBox_live_comments.setText(QCoreApplication.translate("Protocol_Settings", u"allow live comments to protocol", None))
+        self.checkBox_NeXus.setText(QCoreApplication.translate("Protocol_Settings", u"also write NeXus entry to data", None))
+        self.checkBox_json_exp.setText(QCoreApplication.translate("Protocol_Settings", u"Export metadata to json", None))
+#if QT_CONFIG(tooltip)
+        self.label.setToolTip(QCoreApplication.translate("Protocol_Settings", u"<html><head/><body><p>Option to use dynamic variables in file names, evaluated each time the protocol runs:</p><p><span style=\" font-family:'Courier New';\">{time}</span>: Protocol start time (similar to ISO 8601 format).</p><p>Example time format: 2007-08-31_16-47-33+01-00</p><p><span style=\" font-family:'Courier New';\">{session}</span>: Current session name (if provided).</p><p><span style=\" font-family:'Courier New';\">{protocol}</span>: Executed protocol name.</p><p><span style=\" font-family:'Courier New';\">{user}</span>: User name.</p><p><span style=\" font-family:'Courier New';\">{sample_id}</span>: Sample ID.</p><p><span style=\" font-family:'Courier New';\">{sample}</span>: Sample name.</p><p>Note: You can use spaces to seperate the variables.</p></body></html>", None))
+#endif // QT_CONFIG(tooltip)
         self.label.setText(QCoreApplication.translate("Protocol_Settings", u"Filename:", None))
 #if QT_CONFIG(tooltip)
         self.lineEdit_filename.setToolTip(QCoreApplication.translate("Protocol_Settings", u"<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
@@ -327,19 +326,21 @@ class Ui_Protocol_Settings(object):
 "li.unchecked::marker { content: \"\\2610\"; }\n"
 "li.checked::marker { content: \"\\2612\"; }\n"
 "</style></head><body style=\" font-family:'Segoe UI'; font-size:9pt; font-weight:400; font-style:normal;\">\n"
-"<p style=\" margin-top:12px; margin-bottom:12px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">Option to use dynamic variables in file names, evaluated each time the protocol runs:</p>\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">Option to use dynamic variables in file names, evaluated each time the protocol runs:</p>\n"
 "<p style=\" margin-top:12px; margin-bottom:12px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-family:'Courier New';\">{time}</span>: Protocol start time (similar to ISO 8601 format).</p>\n"
-"<p style=\" margin-top:12px; margin-bottom:12px; margin-left:0px; ma"
-                        "rgin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-family:'Courier New';\">{session}</span>: Current session name (if provided).</p>\n"
+"<p style=\" margin-top:12px; margin-bottom:12px; margin-left:0px; marg"
+                        "in-right:0px; -qt-block-indent:0; text-indent:0px;\">Example time format: 2007-08-31_16-47-33+01-00</p>\n"
+"<p style=\" margin-top:12px; margin-bottom:12px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-family:'Courier New';\">{session}</span>: Current session name (if provided).</p>\n"
 "<p style=\" margin-top:12px; margin-bottom:12px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-family:'Courier New';\">{protocol}</span>: Executed protocol name.</p>\n"
 "<p style=\" margin-top:12px; margin-bottom:12px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-family:'Courier New';\">{user}</span>: User name.</p>\n"
 "<p style=\" margin-top:12px; margin-bottom:12px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-family:'Courier New';\">{sample_id}</span>: Sample ID.</p>\n"
-"<p style=\" margin-top:12px; margin-bottom:12px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-family:'Courier New';\">{sample}</span>: Sample name.<br /><br />Example time format: 2007-08-31"
-                        "_16-47-33T01-00</p>\n"
-"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">Note: Use a space to separate variables.</p></body></html>", None))
+"<p style=\" margin-top:12px; margin-bottom:12px; margin-left:0p"
+                        "x; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-family:'Courier New';\">{sample}</span>: Sample name.</p>\n"
+"<p style=\" margin-top:12px; margin-bottom:12px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">Note: You can use spaces to seperate the variables.</p></body></html>", None))
 #endif // QT_CONFIG(tooltip)
         self.lineEdit_filename.setText(QCoreApplication.translate("Protocol_Settings", u"Datafile", None))
         self.lineEdit_filename.setPlaceholderText(QCoreApplication.translate("Protocol_Settings", u"Filename", None))
+        self.label_5.setText(QCoreApplication.translate("Protocol_Settings", u"Descriptions:", None))
         self.tabWidget.setTabText(self.tabWidget.indexOf(self.general), QCoreApplication.translate("Protocol_Settings", u"General Settings", None))
         self.tabWidget.setTabText(self.tabWidget.indexOf(self.plot_widge), QCoreApplication.translate("Protocol_Settings", u"Plots", None))
         self.pushButton_add_variable.setText(QCoreApplication.translate("Protocol_Settings", u"+", None))
@@ -347,14 +348,17 @@ class Ui_Protocol_Settings(object):
         self.label_2.setText(QCoreApplication.translate("Protocol_Settings", u"Variables", None))
         self.pushButton_remove_variable.setText(QCoreApplication.translate("Protocol_Settings", u"-", None))
         self.tabWidget.setTabText(self.tabWidget.indexOf(self.variables), QCoreApplication.translate("Protocol_Settings", u"Variables", None))
-        self.label_7.setText(QCoreApplication.translate("Protocol_Settings", u"Instrument Configuration:", None))
-        self.checkBox_no_config.setText(QCoreApplication.translate("Protocol_Settings", u"do not reconfigure instruments at start", None))
+        self.comboBox_h5.setItemText(0, QCoreApplication.translate("Protocol_Settings", u"write hdf5 during run", None))
+        self.comboBox_h5.setItemText(1, QCoreApplication.translate("Protocol_Settings", u"write hdf5 after run", None))
+
         self.label_6.setText(QCoreApplication.translate("Protocol_Settings", u"Protocol Sharing:", None))
         self.pushButton_instrument_aliases.setText(QCoreApplication.translate("Protocol_Settings", u"Instrument Aliases", None))
-        self.checkBox_perform_at_end.setText(QCoreApplication.translate("Protocol_Settings", u"Perform steps at end of protocol", None))
+        self.label_7.setText(QCoreApplication.translate("Protocol_Settings", u"Instrument Configuration:", None))
+        self.checkBox_no_config.setText(QCoreApplication.translate("Protocol_Settings", u"do not reconfigure instruments at start", None))
         self.label_9.setText(QCoreApplication.translate("Protocol_Settings", u"Data Acquisition:", None))
-        self.label_8.setText(QCoreApplication.translate("Protocol_Settings", u"Protocol Cleanup:", None))
         self.flyer_button.setText(QCoreApplication.translate("Protocol_Settings", u"Asynchronous measurement during protocol", None))
+        self.label_8.setText(QCoreApplication.translate("Protocol_Settings", u"Protocol Cleanup:", None))
+        self.checkBox_perform_at_end.setText(QCoreApplication.translate("Protocol_Settings", u"Perform steps at end of protocol", None))
         self.tabWidget.setTabText(self.tabWidget.indexOf(self.advanced), QCoreApplication.translate("Protocol_Settings", u"Advanced", None))
         self.label_title.setText(QCoreApplication.translate("Protocol_Settings", u"General Configuration", None))
         self.label_4.setText(QCoreApplication.translate("Protocol_Settings", u"Protocol Name:", None))
